0da599555e9ea0b8e2cc98ba72081e3b674dec53
TylerVermeulen <36793@ma-web.nl>
2024-12-13 12:36:43 +0100

    erwins commenting convention

--- Mario/Assets/Scripts/MarioMovement/De Tyler Manier/MovementT.cs
+++ Mario/Assets/Scripts/MarioMovement/De Tyler Manier/MovementT.cs
@@ -5,44 +5,39 @@ using UnityEngine.InputSystem;
 
 public class MovementT : MonoBehaviour
 {
-    private Vector3 velocity;
-    [SerializeField]private float speed;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float speed;
+
     private Vector2 moveinput;
+    private Vector3 velocity;    
     private CharacterController controller;
-    [SerializeField] private float rotationspeed;
+    
     void Awake()
     {
         controller = GetComponent<CharacterController>();
     }
     void Update()
     {
-       // velocity = transform.position * moveinput.y * speed;
-
-       // velocity.y = -9.13f;
-        
-        
-        
-        //transform.Rotate(Vector3.up, moveinput.x * Time.deltaTime * rotationspeed);
-
+        RotateCharacter();
+        MoveCharacter();
+    }
+    public void RotateCharacter()
+    {
         if (moveinput != Vector2.zero)
         {
-            
             Vector3 moveDirection = new Vector3(moveinput.x, 0, moveinput.y);
 
-            
             Quaternion toRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
 
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationspeed * Time.deltaTime);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
         }
-
+    }
+    public void MoveCharacter()
+    {
         float moveSpeed = speed * moveinput.magnitude;
-
         velocity = transform.forward * moveSpeed * Time.deltaTime;
         velocity.y = Physics.gravity.y;
-        Debug.Log(velocity);
-
         controller.Move(velocity * Time.deltaTime);
-
     }
 
     public void MoveInput(InputAction.CallbackContext ctx)
