87ff022647bd223b7c1e2aa58d9ff8a3c5d554a9
eliza <elizanabibaks@gmail.com>
2025-01-15 11:58:48 +0100

    Scene

--- Mario/Assets/MovementT.cs
+++ Mario/Assets/MovementT.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class MovementT : MonoBehaviour
 {
     private Rigidbody rb;
 
