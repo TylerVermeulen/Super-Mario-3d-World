e993efd8a8b1c3c420d77079f62bcb66a0b6c5e7
TylerVermeulen <36793@ma-web.nl>
2025-01-23 12:19:54 +0100

    Revert "Merge branch 'Animation(Placeholder-in-unity)' into Develop"
    This reverts commit 6f11a7a53f7aa66a117c5260391fe7fdc8ec78e3, reversing
    changes made to 525ad1afb16a5ffda80e4545604f1d85455e8f20.

--- Mario/Assets/MovementT.cs
+++ Mario/Assets/MovementT.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DepricatedMovement: MonoBehaviour
+public class MovementT : MonoBehaviour
 {
     private Rigidbody rb;
 
